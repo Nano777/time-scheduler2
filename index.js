ea79b2a1d3827b6453cf5b1e1da1f119feebd9bb
unknown <tatata7956@icloud.com>
2018-11-04 04:18:47 +0900

    my1sttime

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-// import modules
-const express = require('express')
-const request = require('request')
-const bodyParser = require('body-parser')
-
-// create a new express server
-const app = express()
-
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({
-  extended: true
-})) // for parsing application/x-www-form-urlencoded
-
-app.post('/callback', (req, res) => {
-  const options = {
-    method: 'POST',
-    uri: 'https://api.line.me/v2/bot/message/reply',
-    body: {
-      replyToken: req.body.events[0].replyToken,
-      messages: [{
-        type: 'text',
-        text: req.body.events[0].message.text // ここに指定した文字列がボットの発言になる
-      }]
-    },
-    auth: {
-      bearer: 'CaG10hTyBbHkVLRtLZ4B7NCoQ2VkTr7KxlmggfXD3yZvwSP2jD6BjtuTq4I73CLmigmn8q45BO5pZNaXXJQ/jwqKR0dbzoVoVRxddSmCXUe8spQ4H4ji8FDn15+RFBXxNu7aFR8LfGSqUeSV5jeaBgdB04t89/1O/w1cDnyilFU=' // ここは自分のtokenに書き換える
-    },
-    json: true
-  }
-  request(options, (err, response, body) => {
-    console.log(JSON.stringify(response))
-  })
-  res.send('OK')
-})
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('server starting on PORT:' + process.env.PORT)
-})
+var http = require('http'); //httpモジュール呼び出し
+http.createServer(function (request, response) {
+    // リクエストを受けると以下のレスポンスを送信する
+    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+    response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
+}).listen(8080);  //8080番ポートで待ち受け
