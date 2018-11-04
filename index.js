f5b339a44ddb444bed1de7b3b0c132fb1db1b042
unknown <tatata7956@icloud.com>
2018-11-04 16:18:45 +0900

    my1sttime

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 // -----------------------------------------------------------------------------
 // パラメータ設定
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+    channelAccessToken:'b893f338979de6fd50e02a8d97ae3cfb', // 環境変数からアクセストークンをセットしています
+    channelSecret:'CaG10hTyBbHkVLRtLZ4B7NCoQ2VkTr7KxlmggfXD3yZvwSP2jD6BjtuTq4I73CLmigmn8q45BO5pZNaXXJQ/jwqKR0dbzoVoVRxddSmCXUe8spQ4H4ji8FDn15+RFBXxNu7aFR8LfGSqUeSV5jeaBgdB04t89/1O/w1cDnyilFU=' // 環境変数からChannel Secretをセットしています
 };
 
 // -----------------------------------------------------------------------------
