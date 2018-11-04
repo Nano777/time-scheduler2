55b66b1eae52af7080bc64f96eb934f1a969fc5c
unknown <tatata7956@icloud.com>
2018-11-04 16:16:26 +0900

    my1sttime

--- index.js
+++ index.js
@@ -14,10 +14,21 @@ const line_config = {
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
-
+const bot = new line.Client(line_config);
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/callback', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
+	
+	req.body.events.forEach((event) =>{
+		if(event.type == "message" && event.message.type == "text"){
+			if(event.message.text == "こんにちは"){
+				bot.replyMessage(event.replyToken,{
+					type:"text",
+					text:"これはこれは"
+				});
+			}
+		}
+	})
     console.log(req.body);
 });
