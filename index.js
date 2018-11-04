b8fa05992715b0ed567659740c62ae6adf71c311
unknown <tatata7956@icloud.com>
2018-11-05 03:01:53 +0900

    my1sttime

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ function queryDatabase(event, column, condition, callback){
 		if (err) throw err;
 		else {
 			client.query(query,function(error,results){
+				console.log(results);
 				bot.replyMessage(event.replyToken,{
 					type:"text",
 					text:results
