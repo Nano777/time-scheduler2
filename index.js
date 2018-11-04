49d61c531d08563d4ac5e73774555dc1a3ab0341
unknown <tatata7956@icloud.com>
2018-11-04 16:57:03 +0900

    my1sttime

--- index.js
+++ index.js
@@ -3,21 +3,34 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
+const ACCESS_TOKEN = 'CaG10hTyBbHkVLRtLZ4B7NCoQ2VkTr7KxlmggfXD3yZvwSP2jD6BjtuTq4I73CLmigmn8q45BO5pZNaXXJQ/jwqKR0dbzoVoVRxddSmCXUe8spQ4H4ji8FDn15+RFBXxNu7aFR8LfGSqUeSV5jeaBgdB04t89/1O/w1cDnyilFU='
+const CHANNEL_SECRET = 'b893f338979de6fd50e02a8d97ae3cfb'
 // -----------------------------------------------------------------------------
 // パラメータ設定
 const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+    channelAccessToken:ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret:CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
 
 // -----------------------------------------------------------------------------
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
-
+//const bot = new line.Client(line_config);
 // -----------------------------------------------------------------------------
 // ルーター設定
-server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+server.post('/callback', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
+	
+	req.body.events.forEach((event) =>{
+		if(event.type == "message" && event.message.type == "text"){
+			if(event.message.text == "こんにちは"){
+				bot.replyMessage(event.replyToken,{
+					type:"text",
+					text:"これはこれは"
+				});
+			}
+		}
+	})
     console.log(req.body);
 });
