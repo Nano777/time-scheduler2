0e5b938cdeb15f509a12d417732bc9c30313542d
unknown <tatata7956@icloud.com>
2018-11-10 02:58:49 +0900

    my1sttime

--- index.js
+++ index.js
@@ -79,21 +79,21 @@ server.post('/callback', line.middleware(line_config), (req, res, next) => {
 						  "title": "Menu",
 						  "text": "何曜日に追加しますか",
 						  "actions": [
-							  {
+							{
 								"type": "postback",
 								"label": "Buy",
 								"data": "action=buy&itemid=123"
-							  },
-							  {
-								"type": "postback",
-								"label": "Add to cart",
-								"data": "action=add&itemid=123"
-							  },
-							  {
-								"type": "message",
-								"label": "火曜日",
-								"text": "Tuesday"
-							  }
+							},
+							{
+							"type": "postback",
+							"label": "Add to cart",
+							"data": "action=add&itemid=123"
+							},
+							{  
+							"type":"message",
+							"label":"Yes",
+							"text":"Yes"
+							},
 						  ]
 					  }
 					};
