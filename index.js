1703782517b9fc9c2884834692e11f5563db225c
unknown <tatata7956@icloud.com>
2018-11-11 01:37:20 +0900

    my1sttime

--- index.js
+++ index.js
@@ -74,9 +74,9 @@ server.post('/callback', line.middleware(line_config), (req, res, next) => {
 				case /^[1-6]-[1-4]-[月火水木金]曜日-[1-6]-.*-.*/.test(event.message.text):
 					console.log('登録モード')
 					var data = event.message.text.split('-');
-					var values = [data[0], data[1], "'"+data[2]+"'", data[3], "'"+data[4]+"'", "'"+data[5]+"'", "'"+event.source.user_id+"'"]
+					var values = [data[0], data[1], data[2], data[3], data[4], data[5], event.source.user_id]
 					//var query = "INSERT INTO time_schedule VALUES ("+data[0]+","+data[1]+",'"+data[2]+"',"+data[3]+",'"+data[4]+"','"+data[5]+"','"+event.source.user_id+"');"; 
-					var query = 'INSERT INTO time_schedule (grade, quarter, day_of_week, period name, area, userid) VALUES ($1, $2, $3, $4, $5, $6, $7)'
+					var query = 'INSERT INTO time_schedule (grade, quarter, day_of_week, period name, area, userid) VALUES ($1, $2, %3$s, $4, %5$s, %6$s, %7$s)'
 					InsertQuery(data, event, query, values);
 				default:
 					bot.replyMessage(event.replyToken,{
