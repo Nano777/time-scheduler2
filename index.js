71adfb0d9ca0af5bcb5d8d4ac2b86811c88b3c1f
unknown <tatata7956@icloud.com>
2018-11-13 18:35:51 +0900

    my1sttime

--- index.js
+++ index.js
@@ -160,9 +160,9 @@ function SelectQuery(event, table, where, type){
 }
 function InsertQuery(data, event, query, values){
 	var reply = "";
-	var cval = [data[0],data[1],data[2],data[3],data[6]]
+	//var cval = [data[0],data[1],data[2],data[3],data[6]]
 	var check = "select count(*) from time_schedule where grade=$1 AND quarter=%I AND day_of_week=$3 AND period=%I AND userid=%I";
-	var sql = format(check,cval);
+	var sql = format(check,data[0],data[1],data[2],data[3],data[6]);
 	console.log(sql);
 	client.query(check,cval)
 	.then(res=> {
